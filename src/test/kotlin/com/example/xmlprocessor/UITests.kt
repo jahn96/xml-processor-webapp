5c4ddb976865e2444e81e6670bc432adb882e821
jahn163 <jahn163@bloomberg.net>
2024-06-02 18:57:42 -0400

    inject port

--- src/test/kotlin/com/example/xmlprocessor/UITests.kt
+++ src/test/kotlin/com/example/xmlprocessor/UITests.kt
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UITests(
     @Autowired val restTemplate: TestRestTemplate,
+    @LocalServerPort val port: Int,
 ) {
     @BeforeAll
     fun setup() {
@@ -22,7 +24,7 @@ class UITests(
     @Test
     fun `Assert xml processor page title, content and status code`() {
         println(">> Assert xml processor page title, content and status code")
-        val entity = restTemplate.getForEntity<String>("/")
+        val entity = restTemplate.getForEntity<String>("http://localhost:$port/" )
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains("<h1>XmlProcessor</h1>")
     }
